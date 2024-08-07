3f623d1817e3de3386b9a07011ca8867daadbace
hari8924 <140717651+hari8924@users.noreply.github.com>
2024-08-07 12:44:55 +0530

    Add files via upload

---
+++ dags/target/dbt-code/models/netflix/stage/CREDITS_DIM.sql
@@ -0,0 +1,17 @@
+
+  
+    
+
+        create or replace transient table PROD.DBT_TRANSFORM.CREDITS_DIM
+         as
+        (
+
+
+
+SELECT 
+* 
+FROM PROD.DBT_RAW.CREDITS
+WHERE ROLE IN ('ACTOR','DIRECTOR')
+        );
+      
+  
